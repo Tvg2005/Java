08b0800ecab34702b168f84319d3c96ca6f38c63
Thiago Venâncio <142404454+Tvg2005@users.noreply.github.com>
2023-08-23 10:37:04 -0300

    Update Principal.java

--- aula2/src/aula2/Principal.java
+++ aula2/src/aula2/Principal.java
@@ -7,29 +7,29 @@ import javax.swing.JOptionPane;
 public class Principal {
 	
 	 public static void main(String[] args) {
-//		Mercadoria m1 = new Mercadoria();
-//		m1.nome = "Televis„o";
-//		m1.codigo = 123;
-//		m1.preco = 1000.50;
-//		
-//		Scanner in =  new Scanner(System.in);
-//		Mercadoria m2 = new Mercadoria();
-//		
-//		System.out.println("Informe o nome do produto: ");
-//		m2.nome = in.next();
-//		System.out.println("Informe o cÛdigo do produto: ");
-//		m2.codigo = in.nextInt();
-//		System.out.println("Informe o preÁo do produto: ");
-//		m2.preco = in.nextDouble();
-//		
-//		System.out.println(m2.nome);
-//		System.out.println(m2.codigo);
-//		System.out.println(m2.preco);
-//		
+		Mercadoria m1 = new Mercadoria();
+		m1.nome = "Televis√£o";
+		m1.codigo = 123;
+		m1.preco = 1000.50;
+		
+		Scanner in =  new Scanner(System.in);
+		Mercadoria m2 = new Mercadoria();
+		
+		System.out.println("Informe o nome do produto: ");
+		m2.nome = in.next();
+		System.out.println("Informe o c√≥digo do produto: ");
+		m2.codigo = in.nextInt();
+		System.out.println("Informe o pre√ßo do produto: ");
+		m2.preco = in.nextDouble();
+		
+		System.out.println(m2.nome);
+		System.out.println(m2.codigo);
+		System.out.println(m2.preco);
+		
 		Mercadoria m3 = new Mercadoria();
 		
 		m3.nome = JOptionPane.showInputDialog("Informe o nome: ");
-		m3.preco = Double.valueOf(JOptionPane.showInputDialog("Informe o preÁo: "));
+		m3.preco = Double.valueOf(JOptionPane.showInputDialog("Informe o pre√ßo: "));
 		m3.codigo = Integer.valueOf(JOptionPane.showInputDialog("Informe o codigo: "));
 		
 		JOptionPane.showMessageDialog(null, m3.toString());
