fa3f5d7b1d9c414613568cdb185f89aba63a00c2
Thiago Venâncio <142404454+Tvg2005@users.noreply.github.com>
2023-10-18 10:27:33 -0300

    Add files via upload

---
+++ ordenacao/src/aula11/Principal.java
@@ -0,0 +1,32 @@
+package aula11;
+
+public class Principal {
+	public static void main(String[] args) {
+		int numeros[] = { 6, 4, 8, 2, 1, 7 };
+
+		System.out.print("Array original: ");
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.print(numeros[i] + " ");
+		}
+
+		bubbleSort(numeros);
+		System.out.println();
+		System.out.print("Array Ordenado: ");
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.print(numeros[i] + " ");
+		}
+	}
+
+	public static void bubbleSort(int[] arr) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+}
